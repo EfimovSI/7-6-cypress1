f5ee96027c6f5432411af5e94dec9e9bc2167d73
Sergey Efimov <yefimovsergey00@gmail.com>
2022-09-02 21:53:23 +0300

    First test uncommented

--- cypress/integration/homework.cy.js
+++ cypress/integration/homework.cy.js
@@ -16,23 +16,23 @@ beforeEach(() => {
   cy.viewport(Cypress.env('viewportWidth'), Cypress.env('viewportHeight'));
 });
 
-// it('Should successfully add books to list', (login = users.validUsers[0]
-//   .mail, password = users.validUsers[0].pass, title = books.validBooks[0]
-//   .title, description = books.validBooks[0].description, authors = books
-//   .validBooks[0].authors, title1 = books.validBooks[1]
-//   .title, description1 = books.validBooks[1].description, authors1 = books
-//   .validBooks[1].authors, title2 = books.validBooks[2]
-//   .title, description2 = books.validBooks[2].description, authors2 = books
-//   .validBooks[2].authors) => {
-//   cy.visit('/');
-//   cy.login(login, password);
-//   cy.addBookToList(title, description, authors);
-//   cy.addBookToList(title1, description1, authors1);
-//   cy.addBookToList(title2, description2, authors2);
-//   cy.contains(title).should('be.visible');
-//   cy.contains(title1).should('be.visible');
-//   cy.contains(title2).should('be.visible');
-// });
+it('Should successfully add books to list', (login = users.validUsers[0]
+  .mail, password = users.validUsers[0].pass, title = books.validBooks[0]
+  .title, description = books.validBooks[0].description, authors = books
+  .validBooks[0].authors, title1 = books.validBooks[1]
+  .title, description1 = books.validBooks[1].description, authors1 = books
+  .validBooks[1].authors, title2 = books.validBooks[2]
+  .title, description2 = books.validBooks[2].description, authors2 = books
+  .validBooks[2].authors) => {
+  cy.visit('/');
+  cy.login(login, password);
+  cy.addBookToList(title, description, authors);
+  cy.addBookToList(title1, description1, authors1);
+  cy.addBookToList(title2, description2, authors2);
+  cy.contains(title).should('be.visible');
+  cy.contains(title1).should('be.visible');
+  cy.contains(title2).should('be.visible');
+});
 
 it('Should successfully add book to favorits', (login = users.validUsers[1]
   .mail, password = users.validUsers[1].pass, title = books.validBooks[0]
